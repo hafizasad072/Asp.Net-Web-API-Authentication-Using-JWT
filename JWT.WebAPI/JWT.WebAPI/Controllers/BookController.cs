bea3a813e8c24161f77675b1c3c4e7f9de60518c
Asad Muhammad <asad.muhammad@nxb.com.pk>
2018-04-27 11:09:07 +0500

    First Commit

---
+++ JWT.WebAPI/JWT.WebAPI/Controllers/BookController.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Http;
+using WebApi.Jwt.Filters;
+using System.Linq;
+using System.Collections.Generic;
+using System.Web.Http.Cors;
+using HelperModel;
+
+namespace WebApi.Jwt.Controllers
+{
+    [RoutePrefix("api/NXBBook")]
+    [EnableCors(origins: "http://localhost:58823/", headers: "*", methods: "get")]
+    
+    public class BookController : ApiController
+    {
+
+        [HttpGet]
+        [JwtAuthentication]
+        [Route("GetAllBooks")]
+        public IEnumerable<HMBook> AllBooks()
+        {
+            try
+            {
+                using (DB.NXBEntities _context = new DB.NXBEntities())
+                {
+                    return _context.tbl_Book.Select(x => new HMBook { Author = x.Author, BookName = x.BookName, Category = x.Category }).ToList();
+                }
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ JWT.WebAPI/JWT.WebAPI/Controllers/TokenController.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Web.Http;
+using System.Linq;
+using System;
+using HelperModel;
+using System.Web.Http.Cors;
+
+namespace WebApi.Jwt.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    [RoutePrefix("api/Token")]
+    public class TokenController : ApiController
+    {
+
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("GetToken")]
+        public string Post(HMLoginModel data)
+        {
+            if (CheckUser(data))
+            {
+                return JwtManager.GenerateToken(data.Username);
+            }
+
+            throw new HttpResponseException(HttpStatusCode.Unauthorized);
+        }
+        [NonAction]
+        public bool CheckUser(HMLoginModel data)
+        {
+            try
+            {
+                using (DB.NXBEntities _context = new DB.NXBEntities())
+                {
+                    var user = _context.tbl_User.Where(x => x.UserName == data.Username && x.Password == data.Password).FirstOrDefault();
+                    if (user != null)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
